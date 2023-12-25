f6fc3ddd37ffe41425fe4a4a5d99ccc5969b99f4
Rayan Waked <rayan@waked.dev>
2023-12-25 11:22:18 -0800

    Initial Commit

---
+++ FullView/FullViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullViewApp.swift
+//  FullView
+//
+//  Created by Rayan Waked on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FullViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
